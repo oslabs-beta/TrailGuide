6dddbcaad6ef9b50a9a8095ed3cf4cc467cbc21c
Samuel <samlarivs@outlook.com>
2024-10-10 15:33:07 -0400

    reorganizes frontend file structure, adds nav-bar

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar.tsx';
+//import Home from './pages/Home';
+//import Profile from './pages/Profile';
+//import EventsDashboard from './pages/EventsDashboard';
+
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
+{/* <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/events" element={<EventsDashboard />} /> */}
