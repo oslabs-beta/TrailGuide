d88887292637dc42132df1258b174408a78205c7
Samuel <samlarivs@outlook.com>
2024-10-16 19:36:38 -0400

    Feat: adds heatmap of IP access locations based on real user data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import Card from '../components/Card';
-import UserActivityChart from '../components/charts/LineChart';
-import TestPieChart from '../components/charts/PieChart';
+//import UserActivityChart from '../components/charts/LineChart';
+//import TestPieChart from '../components/charts/PieChart';
 import HeatMap from '../components/charts/HeatMap';
 import IpAccessCombined from '../components/IpAccessCombined';
 
 const Home: React.FC<{ isDarkMode: boolean }> = ({ isDarkMode }) => (
-  <main>
-    <Card title="Important Data. Very Important" isDarkMode={isDarkMode}>
-      <TestPieChart />
+  <main className="main">
+    <Card isDarkMode={isDarkMode} title="ACCESS BY IP ADDRESS">
+    <IpAccessCombined />
     </Card>
-    <Card title="Real User Data. I Promise" isDarkMode={isDarkMode}>
-      <UserActivityChart />
-    </Card>
-    <Card title="Test Heat Map" isDarkMode={isDarkMode}>
+    <Card title="IP ORIGIN MAP" isDarkMode={isDarkMode}>
       <HeatMap />
-    </Card>
-    <Card isDarkMode={isDarkMode} title="Access by Ip Address">
-      <IpAccessCombined />
     </Card>
   </main>
 );
 
 export default Home;
+
+{/* <Card title="Important Data. Very Important" isDarkMode={isDarkMode}>
+      <TestPieChart />
+    </Card>
+    <Card title="Real User Data. I Promise" isDarkMode={isDarkMode}>
+      <UserActivityChart />
+    </Card> */}
