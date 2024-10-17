8b2623759618b0d171cfa315e60ff80be7c55f30
Samuel <samlarivs@outlook.com>
2024-10-17 16:11:46 -0400

    Feat: adds all around styling (in-progress)

--- client/src/pages/EventsDashboard.tsx
+++ client/src/pages/EventsDashboard.tsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, lazy } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Modal from '../components/Modal';
 import { CloudTrailEvent, ParsedAWSEvent } from '../types';
 import { EventsDashboardProps } from '../types';
-
+import EventCard from '../components/EventCard';
 import { getEvents } from '../aws/getEvents';
 
-const EventCard = lazy(() => import('../components/EventCard'));
-const Modal = lazy(() => import('../components/Modal'));
+// const EventCard = lazy(() => import('../components/EventCard'));
+// const Modal = lazy(() => import('../components/Modal'));
 
 const EventsDashboard: React.FC<EventsDashboardProps> = ({ isDarkMode }) => {
   const [modalOpen, setModalOpen] = useState(false);
