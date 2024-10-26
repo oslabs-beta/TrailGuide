43b3ce31940435b6a9c8190ddcf11186d80f5408
aidanprior <priorlyaidan@yahoo.com>
2024-10-26 17:32:29 -0400

    feat(CI): update after test

--- eslint.config.js
+++ eslint.config.js
@@ -3,56 +3,44 @@ import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import prettier from 'eslint-plugin-prettier';
-import typescriptParser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    ignores: ['dist'],
-
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: typescriptParser,
-      parserOptions: {
-        project: ['./tsconfig.node.json', './client/tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
     },
-
-    settings: {
-      react: { version: '18.3' },
-    },
-
+    settings: { react: { version: '18.3' } },
     plugins: {
-      '@typescript-eslint': typescriptEslint,
       react: react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier,
     },
-
     rules: {
-      // TypeScript Recommended Rules
-      ...typescriptEslint.configs.recommended.rules,
-      ...typescriptEslint.configs['recommended-type-checked'].rules,
-      ...typescriptEslint.configs['stylistic-type-checked'].rules,
-
-      // React Rules
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-
-      // React Hooks Rules
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
-
-      // Prettier Formatting Rule
-      'prettier/prettier': ['error'],
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
     },
   },
-];
+  {
+    languageOptions: {
+      // other options...
+      parserOptions: {
+        project: ['./tsconfig.node.json', './client/tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  }
+);
