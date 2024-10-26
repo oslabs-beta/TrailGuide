a6cd6b001d1d68e551e504c3e8810a7659e68ee9
Samuel <samlarivs@outlook.com>
2024-10-26 13:04:46 -0400

    Fix: adds dynamic sizing to User activity Yaxis

--- client/src/components/charts/UserActivity.tsx
+++ client/src/components/charts/UserActivity.tsx
@@ -31,9 +31,9 @@ const UserActivityChart: React.FC = () => {
   //reversed the times to show the most recent first
   return (
     <AreaChart width={600} height={300} data={data} margin={{top: -100, left: -5, right:5, bottom:50}}>
-      <CartesianGrid strokeDasharray="3 3" />
+      <CartesianGrid strokeDasharray={"3 3"} />
       <XAxis dataKey="localTime" reversed angle={-45} textAnchor="end" />
-      <YAxis tickCount={10} interval={3}/>
+      <YAxis domain={[0, 'dataMax + 11']}/>
       <Tooltip />
       <Area
         type="monotone"
