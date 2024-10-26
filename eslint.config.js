0ad69b9a0da7ddd08d3487b68c990c54a1909b1c
aidanprior <priorlyaidan@yahoo.com>
2024-10-26 17:04:20 -0400

    feat(CI): linting and formatting workflow

--- eslint.config.js
+++ eslint.config.js
@@ -3,44 +3,56 @@ import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import typescriptParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
+    ignores: ['dist'],
+
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-    },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react: react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-    },
-  },
-  {
-    languageOptions: {
-      // other options...
+      parser: typescriptParser,
       parserOptions: {
         project: ['./tsconfig.node.json', './client/tsconfig.app.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  }
-);
+
+    settings: {
+      react: { version: '18.3' },
+    },
+
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      react: react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+
+    rules: {
+      // TypeScript Recommended Rules
+      ...typescriptEslint.configs.recommended.rules,
+      ...typescriptEslint.configs['recommended-type-checked'].rules,
+      ...typescriptEslint.configs['stylistic-type-checked'].rules,
+
+      // React Rules
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+
+      // React Hooks Rules
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+
+      // Prettier Formatting Rule
+      'prettier/prettier': ['error'],
+    },
+  },
+];
